28/09/16 - Final commit until full writeup -  Fixed some networking issues to allow graceful shutdown and proper network setup over multiple broadcasts. Added static target option to speed up RPP with initial guess, added local target preview window to main, fixed Xwindow threading problem

HSConfig.py
@@ -11,21 +11,21 @@ __author__ = 'Michael Holmes'
 
 # Configuration Filenames
 masterConfigFiles = dict(commonFilename='commonCalib.cfg',
-                         intrinFilename='intrinCalib1.cfg',
-                         extrinFilename='extrinCalib3Node.cfg',
+                         intrinFilename='intrinCalib1-480.cfg',
+                         extrinFilename='extrinCalib3Parallel.cfg',
                          targetFilename='targetCalib.cfg')
 
 slaveConfigFiles = dict(commonFilename='commonCalib.cfg',
-                        intrinFilename='intrinCalib3.cfg')
+                        intrinFilename='intrinCalib3-480.cfg')
 
 # Miscellaneous Parameters
 mainParams = dict(intrinCalibOnly=0,
-                  noNetwork=1,
+                  noNetwork=0,
                   viewLocalTargets=0,
                   maxRayDist=15000)
 
 # Local Tracking Parameters
-trackingParams = dict(numTargets=1,
+trackingParams = dict(numTargets=3,
                       maxFailedCycles=5,
                       deadZone=20,
                       shiftMulti=2)
@@ -43,8 +43,7 @@ netParams = dict(nodeType='Master',
 camParams = dict(camIndex=1,
                  capHeight=480,
                  capWidth=640,
-                 capFrameRate=25.0,
-                 capCodec='XVID')
+                 capFrameRate=30.0)
 
 # Kalman Filter / Sparse Optical Flow Parameters
 kalmanParams = dict(initState=np.zeros((6, 1)),
@@ -54,13 +53,13 @@ kalmanParams = dict(initState=np.zeros((6, 1)),
                     velSTD=10.0,
                     accelSTD=1.5,
                     rangeSTD=6.0,
-                    predictStep=0.1,
-                    updateStep=0.5)
+                    predictStep=0.02,
+                    updateStep=0.1)
 
 flowParams = dict(winSize=(15, 15),
                   maxLevel=2,
                   criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
-                  trackLen=10)
+                  trackLen=20)
 
 featureParams = dict(maxCorners=500,
                      qualityLevel=0.3,
@@ -71,7 +70,7 @@ featureParams = dict(maxCorners=500,
 colourParams = dict(hueWindow=10,
                     satWindow=20,
                     kernelSize=5,
-                    colourTargetRadius=5)
+                    colourTargetRadius=15)
 
 # Initialise Calibration Parameters
 calibCommonParams = dict(refineWindow=(5, 5),
@@ -81,7 +80,7 @@ calibCommonParams = dict(refineWindow=(5, 5),
 calibIntParams = dict(intPatternType='Circle',
                       intPatternSize=(4, 11),
                       intPatternDimension=74,
-                      intNumImages=30,
+                      intNumImages=20,
                       camMatrix=np.zeros((3, 3)),
                       distCoefs=np.zeros((5, 1)))
 
@@ -91,9 +90,10 @@ calibExtParams = dict(extPatternType='Square',
                       markerOffsets=[-0.66, -0.5, 0.66, -0.5, 0.66, 0.5, -0.66, 0.5],
                       gridOffset=0.21,
                       nestMin=4,
-                      objErrTol=2.0,
-                      extCalTime=1.5,
+                      objErrTol=1.0,
+                      extCalTime=15.0,
                       extCapInterval=0.1,
+                      staticTarget=0,
                       markerSides=dict(N=(6, 1),
                                        S=(5, 2),
                                        E=(4, 3),

calibration.py
@@ -25,9 +25,11 @@ __author__ = 'Michael Holmes'
 # Class to handle all intrinsic and extrinsic camera calibration
 # operations.
 #
-# TODO: Add multi-zonal support post-thesis
-# TODO: RPP node loadsharing, move to C++ wrap
+# TODO: Add calibration resolution to intrinCalib and rescale intrinsic matrix to designated resolution
 # TODO: Improve Extrinsic Angular sensitivity - Change extrinsic chessboard to more viewable pattern
+# TODO: Add override for static target flag in extrin_calibration -> set timeweight to max
+# TODO: Add multi-zonal support
+# TODO: RPP node loadsharing, move to C++ wrap
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
@@ -45,7 +47,7 @@ def onMouse(event, x, y, flags, param):
 
 class Calibration:
     def __init__(self, camRef, netRef, commonFilename=None, intrinFilename=None, extrinFilename=None,
-                 targetFilename=None, noNetwork=False):
+                 targetFilename=None, noNetwork=False, intrinOnly=False):
         self._camRef = camRef
         self._netRef = netRef
         self._intPatternType = None
@@ -67,6 +69,7 @@ class Calibration:
         self._objErrTol = None
         self.extCalTime = None
         self._extCapInterval = None
+        self._staticTarget = None
         # offsets list as a percentage of the length of the circular grid longest sides for each marker corner,
         # clockwise from top left
         self._markerOffsets = None
@@ -116,6 +119,8 @@ class Calibration:
                 if ret:
                     self._write_intrin_to_file('intrinCalib.cfg')
                     print 'Written intrinsic calibration file to <intrinCalib.cfg>.'
+                    if intrinOnly:
+                        return
                 else:
                     print 'Cannot continue without calibration.'
                     exit()
@@ -262,8 +267,8 @@ class Calibration:
     def _members_from_params(self, fileType, intPatternType=None, extPatternType=None, intPatternSize=None,
                              extPatternSize=None, intPatternDimension=None, extPatternDimension=None, intNumImages=None,
                              refineWindow=None, numIter=None, epsIter=None, camMatrix=None, distCoefs=None,
-                             nestMin=None, objErrTol=None, extCalTime=None, extCapInterval=None, markerOffsets=None,
-                             gridOffset=None, markerSides=None, extrinTransforms=None):
+                             nestMin=None, objErrTol=None, extCalTime=None, extCapInterval=None, staticTarget=None,
+                             markerOffsets=None, gridOffset=None, markerSides=None, extrinTransforms=None):
         if fileType == 'common':
             self._refineWindow = refineWindow
             self._numIter = numIter
@@ -284,6 +289,7 @@ class Calibration:
             self._objErrTol = objErrTol
             self.extCalTime = extCalTime
             self._extCapInterval = extCapInterval
+            self._staticTarget = staticTarget
             self._markerOffsets = markerOffsets
             self._gridOffset = gridOffset
             self._markerSides = markerSides
@@ -359,6 +365,7 @@ class Calibration:
                            nestMin=self._nestMin,
                            extCalTime=self.extCalTime,
                            extCapInterval=self._extCapInterval,
+                           staticTarget=self._staticTarget,
                            markerOffsets=self._markerOffsets,
                            gridOffset=self._gridOffset,
                            markerSides=self._markerSides,
@@ -435,13 +442,13 @@ class Calibration:
         objPArray = []
         imgPArray = []
         calibCount = 0
-        self._camRef.get_frame()
-        h, w = self._camRef.current_frame()[0].shape[:2]
+        currFrame = self._camRef.get_frame()
+        h, w = currFrame[0].shape[:2]
         cv2.namedWindow('Calibration Capture')
         # Capture calibration images
         while calibCount < self._intNumImages:
-            self._camRef.get_frame()
-            frameCopy = self._camRef.current_frame()[0]
+            currFrame = self._camRef.get_frame()
+            frameCopy = currFrame[0]
             cv2.imshow('Calibration Capture', frameCopy)
             userIn = cv2.waitKey(50)
             ret = None
@@ -577,9 +584,6 @@ class Calibration:
     def _calibrate_ext_slave(self):
         if __debug__:
             print 'Running in DEBUG MODE!'
-        # Setup capture file and timing list
-        # codec = cv2.VideoWriter_fourcc(*mP['capCodec'])
-        # capFile = cv2.VideoWriter('extrinSlaveCap.avi', codec, mP['capFrameRate'], (mP['capWidth'], mP['capHeight']))
         imgList = []
         timeList = []
 
@@ -589,18 +593,17 @@ class Calibration:
         startTime = time.time()
         currTime = startTime
         lastCheckTime = startTime - self._extCapInterval - 1
+        lastFrameID = '0'
         while currTime - startTime < self.extCalTime:
-            self._camRef.get_frame()
-            frameCopy = self._camRef.current_frame()[0]
+            currFrame = self._camRef.get_frame()
+            frameCopy = currFrame[0]
+            newFrameID = currFrame[1]
             currTime = time.time()
-            if currTime - lastCheckTime > self._extCapInterval:
-                # capFile.write(frameCopy)
+            if currTime - lastCheckTime > self._extCapInterval and newFrameID != lastFrameID:
+                lastFrameID = newFrameID
                 imgList.append(cv2.cvtColor(frameCopy, cv2.COLOR_BGR2GRAY))
                 timeList.append(currTime)
                 lastCheckTime = time.time()
-
-        # Release file for reading
-        # capFile.release()
 
         frameCount = 0
         numFrames = len(imgList)
@@ -634,6 +637,8 @@ class Calibration:
                             cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
                 cv2.imshow('Extrinsic Calibration', frameCopy)
                 cv2.waitKey(100)
+            else:
+                print 'Frame %d/%d\tUnable to find grid.' % (frameCount, numFrames)
 
         if __debug__:
             cv2.destroyWindow('Extrinsic Calibration')
@@ -655,18 +660,30 @@ class Calibration:
             reducedObjPoints = self._outside_corners(objPoints).T
             numTargets = len(foundTargets)
             targetCount = 0
+            lastRot = None
+            lastGoodRot = None
+            lastCapTime = 0
             for side, corners, capTime in foundTargets:
                 reducedCorners = self._outside_corners(corners)
-                rot, trans, objErr = self._solve_rpp(reducedObjPoints, reducedCorners)
+                if lastRot is not None and capTime - lastCapTime < 2.0 * self._extCapInterval:
+                    rot, trans, objErr = self._solve_rpp(reducedObjPoints, reducedCorners, initR=lastRot)
+                elif lastGoodRot is not None and self._staticTarget:
+                    rot, trans, objErr = self._solve_rpp(reducedObjPoints, reducedCorners, initR=lastGoodRot)
+                else:
+                    rot, trans, objErr = self._solve_rpp(reducedObjPoints, reducedCorners)
                 targetCount += 1
                 print 'Frame %d/%d Processed.' % (targetCount, numTargets)
                 if objErr < self._objErrTol:
                     transform = np.hstack((np.vstack((rot, np.zeros((1, 3)))), np.vstack((trans, 1))))
                     returnTargets.append((side, transform, capTime, objErr))
                     print 'Frame %d ObjErr = %f < Tolerance %f, valid frame.' % (targetCount, objErr, self._objErrTol)
+                    lastRot = rot.copy()
+                    lastGoodRot = rot.copy()
+                    lastCapTime = float(capTime)
                 else:
                     print 'Frame %d ObjErr = %f > Tolerance %f, discarded frame.' % (
                     targetCount, objErr, self._objErrTol)
+                    lastRot = None
             return returnTargets
         else:
             print 'Unable to find valid frames in scene capture.'
@@ -704,8 +721,8 @@ class Calibration:
         targetSides = []
         capImages = self._intNumImages
         while True and capImages:
-            self._camRef.get_frame()
-            frameCopy = self._camRef.current_frame()[0]
+            currFrame = self._camRef.get_frame()
+            frameCopy = currFrame[0]
             cv2.imshow('Target Calibration', frameCopy)
             userIn = cv2.waitKey(50)
             if userIn & 0xFF == ord('c'):
@@ -768,7 +785,8 @@ class Calibration:
                 break
 
         # Calibrate Sides wrt North face
-        greyFrame = self._camRef.current_frame()[0]
+        currFrame = self._camRef.get_frame()
+        greyFrame = currFrame[0]
         greyFrame = cv2.cvtColor(greyFrame, cv2.COLOR_BGR2GRAY)
         cv2.putText(greyFrame, 'Performing target calibration...', (10, greyFrame.shape[0] - 10),
                     cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
@@ -1236,11 +1254,14 @@ class Calibration:
         return returnedPoints.astype('float32')
 
     # Return pose (R, t, objErr) from input 3xn object points and 2xn image points
-    def _solve_rpp(self, objPoints, imgPoints):
+    def _solve_rpp(self, objPoints, imgPoints, initR=None):
         imgPointsUndistorted = cv2.undistortPoints(imgPoints, self.camMatrix, self.distCoefs)
         imgPointsUndistorted = np.hstack(
             (imgPointsUndistorted[:, 0, 0].reshape((-1, 1)), imgPointsUndistorted[:, 0, 1].reshape((-1, 1)))).T
-        return rpp(objPoints.copy(), imgPointsUndistorted)
+        if initR is None:
+            return rpp(objPoints.copy(), imgPointsUndistorted)
+        else:
+            return rpp(objPoints.copy(), imgPointsUndistorted, initRGuess=initR)
 
     # Takes collated data from every node and performs the final extrinsic calibration
     def _extrin_calibration(self, extrinData, binSize):
@@ -1490,5 +1511,3 @@ if __name__ == '__main__':
     # calib._write_extrin_to_file('extrinCalib.cfg')
     # ret, params = calib._read_extrin_from_file('extrinCalib.cfg')
     # calib._members_from_params('extrin', **params)
-
-    # Straight On Test 180deg, Host 77cm, 27cm; Slave 74cm, 27cm, Box 30cm

localTracking.py
@@ -4,6 +4,7 @@ from scipy import spatial
 import threading
 from kalmanFilt import KalmanTrack
 from HSConfig import kalmanParams, flowParams, featureParams, colourParams, trackingParams
+import time
 
 __author__ = 'Michael Holmes'
 
@@ -15,6 +16,7 @@ __author__ = 'Michael Holmes'
 # TODO: Alternative markup method (non-window) + target save
 # TODO: Outlier rejection in minimum enclosing circle calc
 # TODO: Add toggle for showing track history
+# TODO: Downsampling and Predictive Image Slicing
 # ----------------------------------------------------------------
 
 
@@ -39,10 +41,19 @@ def onMouse(event, x, y, flags, param):
 
 
 class LocalModeOne(KalmanTrack, threading.Thread):
-    def __init__(self, camRef, targetNumber):
+    def __init__(self, camRef, targetNumber, noMarkup=False):
         threading.Thread.__init__(self)
 
-        # Markup Target
+        # Temp timing variables for results
+        self.testTime = 0.
+        self.predictLoops = 0.
+        self.updateLoops = 0.
+        self.bothLoops = 0
+        self.predictRate = 0.
+        self.updateRate = 0.
+        self.bothRate = 0.
+
+        # Markup initialisation
         self._camRef = camRef
         KalmanTrack.__init__(self, **kalmanParams)
         self._currObs = np.zeros((2, 1))
@@ -55,15 +66,17 @@ class LocalModeOne(KalmanTrack, threading.Thread):
         self._colourTargetRadius = colourParams['colourTargetRadius']
         self._flowLock = False
         self._flowTrackHistoryMax = flowParams['trackLen']
-        del flowParams['trackLen']
         self._flowTracks = []
-        self._flowParams = flowParams
+        self._flowParams = flowParams.copy()
+        del self._flowParams['trackLen']
         self._featureParams = featureParams
 
         # Markup Target
         self._hsvLimits = []
-        self._markup_target()
-        del self._colourParams
+        if noMarkup:
+            self._markup_target(noMarkup=True)
+        else:
+            self._markup_target()
 
         # Update filter with initial positions, image resolution and target colour profile
         self.x[0] = self._currObs[0]
@@ -77,6 +90,8 @@ class LocalModeOne(KalmanTrack, threading.Thread):
         self._deadZone = trackingParams['deadZone']
         self._shiftMulti = trackingParams['shiftMulti']
 
+        # FrameID param to check if new image or not
+        self._lastFrameID = '0'
         # Thread params
         self._killThread = False
         self._trackingLock = threading.Lock()
@@ -87,12 +102,24 @@ class LocalModeOne(KalmanTrack, threading.Thread):
         return
 
     def run(self):
+        # Temp results variable
+        self.testTime = time.time()
+        # Thread Loop
         while not self._killThread and self._camRef.is_opened():
-            # Run local tracker, tracking target position with the filter
+            # Temp results variables
+            predictFlag = False
+            updateFlag = False
 
+            # Run local tracker, tracking target position with the filter
             # Get new image
-            self._camRef.get_frame()
-            currFrame, currTimestamp, currDeltaTime = self._camRef.current_frame()
+            currFrame, currFrameID, currTimestamp, currDeltaTime = self._camRef.get_frame()
+
+            # Check if acquired frame is different from last successful loop
+            if currFrameID == self._lastFrameID:
+                continue
+            else:
+                self._lastFrameID = currFrameID
+
             # Update tracker elapsed times
             self.update_elapsed_counters(currDeltaTime)
 
@@ -102,17 +129,20 @@ class LocalModeOne(KalmanTrack, threading.Thread):
                 self._currTimestamp = currTimestamp
                 self.predict()
                 self._targetRead = False
+                predictFlag = True
                 self._trackingLock.release()
 
-            # Run update stage if update increment has elapsed
+            # Run update stage if update increment has elapsed and new frame is acquired
             if self.update_stage_elapsed():
                 self._new_obs_from_im(currFrame.copy())
                 if self.targetLocked:
                     self._trackingLock.acquire()
                     self._currTimestamp = currTimestamp
-                    self.update(self.get_current_obs())
+                    self.update(self._currObs)
                     self._targetRead = False
+                    updateFlag = True
                     self._trackingLock.release()
+
 
             # Push latest filter estimate to image window along with new image
             # if __debug__:
@@ -127,6 +157,13 @@ class LocalModeOne(KalmanTrack, threading.Thread):
                     #     titleString = 'Target %d' % self._targetNumber
                     #     cv2.imshow(titleString, frameCopy)
 
+            if predictFlag and updateFlag:
+                self.bothLoops += 1
+            elif predictFlag:
+                self.predictLoops += 1
+            elif updateFlag:
+                self.updateLoops += 1
+        self.testTime = time.time() - self.testTime
         return
 
     def stop(self):
@@ -144,32 +181,36 @@ class LocalModeOne(KalmanTrack, threading.Thread):
             packet = None
         return packet
 
-    def _markup_target(self):
-        global target0, frameImg
-        cv2.waitKey(3000)
+    def _markup_target(self, noMarkup=False):
+        global target0
+        target0 = [(-1, -1), (-1, -1), False, (-1, -1)]
+        cv2.waitKey(1000)
         self._camRef.get_frame()
         self._camRef.get_frame()  # Doubled to initialise time-step
         currFrame = self._camRef.current_frame()[0]
         self.maxUV = (currFrame.shape[1], currFrame.shape[0])
 
         # Select target region
-        cv2.imshow('Choose Target', currFrame)
-        cv2.setMouseCallback('Choose Target', onMouse)
-        while target0[1][0] == -1:
-            currFrame = self._camRef.current_frame()[0]
-            if not target0[2]:
-                cv2.imshow('Choose Target', currFrame)
-                self._camRef.get_frame()
-            else:
-                if not (target0[3][0] == -1):
-                    frameCopy = currFrame.copy()
-                    cv2.rectangle(frameCopy, target0[0], target0[3], (0, 255, 0))
-                    cv2.imshow('Choose Target', frameCopy)
-                else:
+        if noMarkup:
+            markupPoint = (int(self.maxUV[0] / 2), int(self.maxUV[1] / 2))
+            target0 = [markupPoint, markupPoint, False, (-1, -1)]
+        else:
+            cv2.imshow('Choose Target', currFrame)
+            cv2.setMouseCallback('Choose Target', onMouse)
+            while target0[1][0] == -1:
+                currFrame = self._camRef.get_frame()[0]
+                if not target0[2]:
                     cv2.imshow('Choose Target', currFrame)
-            if cv2.waitKey(50) & 0xFF == ord(' '):
-                cv2.destroyWindow('Choose Target')
-                return
+                else:
+                    if not (target0[3][0] == -1):
+                        frameCopy = currFrame.copy()
+                        cv2.rectangle(frameCopy, target0[0], target0[3], (0, 255, 0))
+                        cv2.imshow('Choose Target', frameCopy)
+                    else:
+                        cv2.imshow('Choose Target', currFrame)
+                if cv2.waitKey(50) & 0xFF == ord(' '):
+                    cv2.destroyWindow('Choose Target')
+                    return
         bounds = self._set_roi(target0)
         self._currObs[0] = bounds[2]
         self._currObs[1] = bounds[5]
@@ -204,7 +245,7 @@ class LocalModeOne(KalmanTrack, threading.Thread):
 
     def _set_hsv_limits(self, bgr):
         # Correct for lone pixel selection
-        if len(bgr) == 3:
+        if bgr.size == 3:
             bgr = bgr.reshape(1, 1, 3)
         hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
         hueMean = np.mean(hsv[:, :, 0])
@@ -230,8 +271,12 @@ class LocalModeOne(KalmanTrack, threading.Thread):
             sat = 255
         return int(sat)
 
-    def get_current_obs(self):
-        return self._currObs
+    def get_current_estimate(self):
+        self._trackingLock.acquire()
+        currX = self.x[0:2]
+        currLock = self.targetLocked
+        self._trackingLock.release()
+        return currX, currLock
 
     def _new_obs_from_im(self, image):
         # Get new measurement from colour extraction
@@ -359,3 +404,66 @@ class LocalModeOne(KalmanTrack, threading.Thread):
 
         self._flowLock = False
         return None
+
+
+if __name__ == '__main__':
+    from camOps import CamHandler
+
+    myCam = CamHandler(1)
+    numTests = 20
+    maxTargets = 5
+    fd = open('localModeResults.txt', 'w')
+    fd.write(str(numTests) + '\n')
+    fd.write(str(maxTargets) + '\n')
+    for targNum in range(maxTargets):
+        print 'Num Targets %d' % (targNum + 1)
+        fd.write(str(targNum + 1) + '\n')
+        meanPredictRate = [x for x in range(targNum + 1)]
+        meanPredictCount = list(meanPredictRate)
+        meanUpdateRate = list(meanPredictRate)
+        meanUpdateCount = list(meanPredictRate)
+        meanBothRate = list(meanPredictRate)
+        meanBothCount = list(meanPredictRate)
+        for idx in range(numTests):
+            myTrackers = []
+            for idx2 in range(targNum + 1):
+                myTrackers.append(LocalModeOne(myCam, 0, noMarkup=True))
+            startTime = time.time()
+            currTime = startTime
+            while currTime - startTime < 5.0:
+                cv2.waitKey(50)
+                currTime = time.time()
+            for tracker in myTrackers:
+                tracker.stop()
+                while tracker.isAlive():
+                    pass
+
+            print 'Test #%d/%d' % (idx + 1, numTests)
+
+            for idx, tracker in enumerate(myTrackers):
+                tracker.predictRate = 0 if tracker.predictLoops == 0 else (
+                1 / (tracker.testTime / tracker.predictLoops))
+                tracker.updateRate = 0 if tracker.updateLoops == 0 else (1 / (tracker.testTime / tracker.updateLoops))
+                tracker.bothRate = 0 if tracker.bothLoops == 0 else (1 / (tracker.testTime / tracker.bothLoops))
+
+                print 'Loop Rates (Predict, Update, Both): %f/%f/%f Hz' % (
+                tracker.predictRate, tracker.updateRate, tracker.bothRate)
+                if tracker.predictRate != 0:
+                    meanPredictRate[idx] += tracker.predictRate
+                    meanPredictCount[idx] += 1
+                if tracker.updateRate != 0:
+                    meanUpdateRate[idx] += tracker.updateRate
+                    meanUpdateCount[idx] += 1
+                if tracker.bothRate != 0:
+                    meanBothRate[idx] += tracker.bothRate
+                    meanBothCount[idx] += 1
+
+        for idx in range(len(meanPredictCount)):
+            meanPredictRate[idx] = 0 if meanPredictCount[idx] == 0 else meanPredictRate[idx] / meanPredictCount[idx]
+            meanUpdateRate[idx] = 0 if meanUpdateCount[idx] == 0 else meanUpdateRate[idx] / meanUpdateCount[idx]
+            meanBothRate[idx] = 0 if meanBothCount[idx] == 0 else meanBothRate[idx] / meanBothCount[idx]
+            print 'Mean Loop Rates (Predict, Update, Both, Count): %f/%f/%f Hz' % (
+            meanPredictRate[idx], meanUpdateRate[idx], meanBothRate[idx])
+            fd.write(str(meanPredictRate[idx]) + ',' + str(meanUpdateRate[idx]) + ',' + str(meanBothRate[idx]) + '\n')
+    fd.close()
+    myCam.stop()

reconstruction.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import threading
 import time
+from os import path
 from itertools import combinations
 from sys import exit
 from camOps import CamHandler
@@ -40,7 +41,7 @@ class Reconstruction(threading.Thread):
         # Reconstruction Params
         self.name = nodeType
         self.posList = []
-        self.rayMax = mainParams['maxRayDist']  # Maximum viewing range for each camera
+        self.rayMax = mainParams['maxRayDist']  # Maximum viewing range for each camera in mm
 
         # Run no-network calibration load
         if noNetwork and nodeType == 'Master':
@@ -55,7 +56,7 @@ class Reconstruction(threading.Thread):
         # Run intrinsic calibration only
         if mainParams['intrinCalibOnly']:
             print 'Running forced intrinsic calibration.'
-            self._calib = Calibration(self._cam, None, noNetwork=True)
+            self._calib = Calibration(self._cam, None, intrinOnly=True)
             # Close camera
             self._cam.stop()
             return
@@ -101,6 +102,7 @@ class Reconstruction(threading.Thread):
     def run(self):
         if self._net.name == 'Master':  # Master loop
             while not self._killThread:
+
                 # If capture flag load tracking data from slaves
                 if self._net.netEventDict['capture'].isSet():
                     self._master_append_local_tracker_data()
@@ -108,21 +110,61 @@ class Reconstruction(threading.Thread):
                 # If recon flag reconstruct from collected tracking data
                 if self._net.netEventDict['reconstruct'].isSet():
                     compiledData = self._master_compile_data()
-                    # savedData = shelve.open('reconData')
-                    # savedData['reconData'] = compiledData
-                    # savedData.close()
                     posData = self._master_reconstruct(compiledData, 0.1)
+                    self._write_recon_to_file(posData)
                     self.toggle_reconstruct()
-                    pass
         else:  # Slave loop
             while not self._killThread:
+                # Check see if a shutdown has been ordered by Master
+                if not self._net.isAlive():
+                    self.shutdown()
                 # If capture flag start transmitting positions
                 if self._net.netEventDict['capture'].isSet():
                     self._slave_send_tracker_data()
                 elif self._net.netEventDict['reconSync'].isSet():
                     self._net.send_comms('Success')
                     self._net.netEventDict['reconSync'].clear()
+        return
 
+    def collect_estimates(self):
+        currFrame = self._cam.get_frame()[0]
+        trackerEstimates = []
+        for tracker in self._trackers:
+            trackerEstimates.append(tracker.get_current_estimate())
+        for estimate, lockStatus in trackerEstimates:
+            if lockStatus:
+                cv2.circle(currFrame, (estimate[0], estimate[1]), 20, (0, 255, 0), 3)
+            else:
+                cv2.circle(currFrame, (estimate[0], estimate[1]), 20, (0, 0, 255), 3)
+        return currFrame
+
+    def _write_recon_to_file(self, posData, filename=None, append=False):
+        # Use default if no name given
+        if filename is None:
+            filename = 'reconstruction.csv'
+        # Open or append unique file for processing
+        if append:
+            fd = open(filename, 'a')
+        else:
+            while path.isfile(filename):
+                splitName = filename.split('.')
+                scoreIdx = splitName[0][::-1].find('_')
+                if scoreIdx != -1:
+                    counter = int(splitName[0][::-1].split('_')[0][::-1]) + 1
+                    filename = splitName[0][:-(scoreIdx + 1)] + '_' + str(counter) + '.csv'
+                else:
+                    counter = 1
+                    filename = splitName[0] + '_1' + '.csv'
+            fd = open(filename, 'w')
+        # Dump CSV variables to file
+        fd.write(time.ctime() + '\n')
+        for targNum, capTime, position in posData:
+            writeStr = repr(targNum) + ',' + repr(capTime)
+            for idx in range(len(position)):
+                writeStr += ',' + repr(position[idx])
+            writeStr += '\n'
+            fd.write(writeStr)
+        fd.close()
         return
 
     def _master_compile_data(self):
@@ -265,7 +307,7 @@ class Reconstruction(threading.Thread):
         if self._net.name == 'Slave':
             return False
         if self._net.netEventDict['reconstruct'].isSet():
-            print 'Finishing reconstruction.'
+            print 'Finished reconstruction.'
             self._net.netEventDict['reconstruct'].clear()
         else:
             print 'Starting reconstruction.'
@@ -357,14 +399,11 @@ class Reconstruction(threading.Thread):
             for idx1, idx2 in combinations(range(len(refinedObsGroup)), 2):
                 line1 = refinedObsGroup[idx1][3]
                 line2 = refinedObsGroup[idx2][3]
-                plane1 = self._calib.extrinPlanes[refinedObsGroup[idx1][1]]
-                plane2 = self._calib.extrinPlanes[refinedObsGroup[idx2][1]]
                 weighting1 = refinedObsGroup[idx1][4]
                 weighting2 = refinedObsGroup[idx2][4]
                 capTime1 = refinedObsGroup[idx1][2]
                 capTime2 = refinedObsGroup[idx2][2]
-                # pairPosEstimate = point_between_skew_lines(line1, line2, weighting1, weighting2)
-                pairPosEstimate = self._point_between_skew_rays(line1, line2, plane1, plane2, weighting1, weighting2)
+                pairPosEstimate = self._point_between_skew_rays(line1, line2, weighting1, weighting2)
                 if pairPosEstimate is not None:
                     avgCapTime = np.average([capTime1, capTime2], weights=[weighting1, weighting2])
                     avgWeight = np.mean([weighting1, weighting2])
@@ -380,19 +419,16 @@ class Reconstruction(threading.Thread):
             avgPosition = np.average(positions[:, 1:].reshape((3, -1)), axis=1, weights=weights)
             avgTime = np.average(capTimes, weights=weights)
             # Append new observation position
-            reconstructedPoints.append((refinedObsGroup[0][0], avgPosition, avgTime))
-
+            reconstructedPoints.append((refinedObsGroup[0][0], avgTime, avgPosition))
         return reconstructedPoints
 
     # Credit Dan Sunday, geomalgorithms.com
-    def _point_between_skew_rays(self, line1, line2, plane1, plane2, weighting1=None, weighting2=None):
+    def _point_between_skew_rays(self, line1, line2, weighting1=None, weighting2=None):
         eps = 1e-8
-        # Find intersection of lines and planes
-        line1Point2 = self._line_plane_intersect(line1, plane2)
-        line2Point2 = self._line_plane_intersect(line2, plane1)
-        # Return None if no valid segment endpoints
-        if line1Point2 is None or line2Point2 is None:
-            return None
+        # Extend ray out to max viewing distance
+        line1Point2 = self._extend_ray(line1)
+        line2Point2 = self._extend_ray(line2)
+
         line1Point1 = line1[:, 0].astype(np.float64)
         line2Point1 = line2[:, 0].astype(np.float64)
 
@@ -464,35 +500,22 @@ class Reconstruction(threading.Thread):
 
         return (close1 + weighting * (close2 - close1)).reshape((3, 1))
 
-    def _line_plane_intersect(self, line, plane):
-        eps = 1e-8
+    def _extend_ray(self, line):
         pointLine = line[:, 0].astype(np.float64)
-        pointPlane = plane[:, 0].astype(np.float64)
         vectLine = line[:, 1].astype(np.float64)
-        normPlane = plane[:, 1].astype(np.float64)
-
-        w = pointLine - pointPlane
-        D = np.dot(normPlane, vectLine)
-        N = -np.dot(normPlane, w)
-        # Line is either in plane or parallel to it
-        if np.fabs(D) < eps:
-            return None
-        sI = N / D
-        if sI > 0.0:  # Return segment intersection with plane
-            return (pointLine + sI * vectLine)
-        else:  # Segment intersects behind camera, employ max range instead
-            sI = self.rayMax / np.linalg.norm(vectLine)
-            return (pointLine + sI * vectLine)
+        sI = self.rayMax / np.linalg.norm(vectLine)
+        return pointLine + sI * vectLine
 
 
 if __name__ == '__main__':
     # Test script for the reconstruction module
-    savedData = shelve.open('reconData3Node')
-    compiledData = savedData['reconData']
-    savedData.close()
-
-    # visualHdl = Visualiser()
+    # savedData = shelve.open('compiledData')
+    # compiledData = savedData['compiledData']
+    # savedData.close()
+    #
+    #
+    # # # visualHdl = Visualiser()
     reconHdl = Reconstruction('Master', noNetwork=True)
-    posData = reconHdl._master_reconstruct(compiledData, 0.1)
+    # posData = reconHdl._master_reconstruct(compiledData, 0.1)
 
     pass

rpp.py
@@ -13,16 +13,15 @@ from rpputil import *
 # Output:   Numpy Tuple (R, t, err) where R = 3x3 Rotation matrix, t = 3x1 translation vector, err = projection error
 def rpp(objPoints=None, imgPoints=None, initRGuess=None, tolerance=None, epsilon=None, rotMethod=None, maxIter=None):
     # Use test data if no values given
-    if objPoints is None:
+    if objPoints is None or imgPoints is None:
         objPoints = np.array([0.0685, 0.6383, 0.4558, 0.7411, -0.7219, 0.7081, 0.7061, 0.2887, -0.9521, -0.2553,
                               0.4636, 0.0159, -0.1010, 0.2817, 0.6638, 0.1582, 0.3925, -0.7954, 0.6965, -0.7795],
                              dtype=np.float64).reshape((2, 10))
         objPoints = np.vstack((objPoints, np.zeros((1, objPoints.shape[1]))))
-
-    if imgPoints is None:
         imgPoints = np.array([-0.0168, 0.0377, 0.0277, 0.0373, -0.0824, 0.0386, 0.0317, 0.0360, -0.1015, -0.0080,
                               0.0866, 0.1179, 0.1233, 0.1035, 0.0667, 0.1102, 0.0969, 0.1660, 0.0622, 0.1608],
                              dtype=np.float64).reshape((2, 10))
+
 
     # Use defaults if no tolerance/epsilon/rotation method set set
     if tolerance is None:
@@ -434,4 +433,8 @@ def get_rotation_Y_wrt_T(v, p, t, DB, Rz=None):
 
 
 if __name__ == '__main__':
-    sol = rpp()
+    R = np.array([0.85762848, -0.31178758, 0.40897664,
+                  0.16046857, -0.59330811, -0.78881894,
+                  0.48859311, 0.74214149, -0.4588058]).reshape((3, 3))
+    sol = rpp(initRGuess=R)
+    pass